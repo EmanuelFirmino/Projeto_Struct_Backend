f6b34f9c9fe39a83850845fcf306a11244ce10ad
EmanuelFirmino <ddos.emanuel@gmail.com>
2021-11-16 19:32:22 -0300

    Controller User

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::UserController < ApplicationController
+    def index
+        users = User.all
+        render json: users, status: 200
+    end
+
+    def create
+        user = User.new(user_params)
+        user.save!
+        render json: user, status: 201
+    rescue StandardError
+        head(:unprocessable_entity)
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user, status 200
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update!(user_params)
+        render json: user status 200
+    rescue StandardError
+        head(:unprocessable_entity)
+    end
+
+    def delete
+        user = User.find(params[:id])
+        user.destroy!
+        render json: user, status 200
+    rescue StandardError
+        head(:bad_request)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :profile_pic)
+end
