41b0d7bf16ca2e1cb7977dfe5b039290d8f2d761
EmanuelFirmino <88447722+EmanuelFirmino@users.noreply.github.com>
2021-11-18 13:06:27 -0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :name, :email, :password, :is_admin, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
