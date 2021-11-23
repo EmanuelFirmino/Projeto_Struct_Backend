81659050ec05b8686c96430af07961ab71168e34
arthur <arthur.lacava01@gmail.com>
2021-11-23 16:39:53 -0300

    integracao geral

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_11_21_200457) do
-
-
-
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -60,7 +55,6 @@ ActiveRecord::Schema.define(version: 2021_11_21_200457) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -95,10 +89,8 @@ ActiveRecord::Schema.define(version: 2021_11_21_200457) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "favorites", "products"
   add_foreign_key "favorites", "users"
-
   add_foreign_key "relations", "categories"
   add_foreign_key "relations", "products"
 end
