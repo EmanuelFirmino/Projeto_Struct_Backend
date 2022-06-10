aa2468a9da3a9846b55e63788c42b3b98bc877fc
Victhor-Alexis <victhoralexis123@gmail.com>
2022-06-10 12:48:28 -0300

    Change cors origins source

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'http://localhost:2000'
+     origins 'https://padoca.ml'
 
      resource '*',
        headers: :any,
