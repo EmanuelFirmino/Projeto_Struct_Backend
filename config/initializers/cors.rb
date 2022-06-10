252c64b949bc69fdff8436554704b1395cbf187b
Victhor-Alexis <victhoralexis123@gmail.com>
2022-06-10 12:55:20 -0300

    Testing with all origins allowed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://oyster-app-waicl.ondigitalocean.app/'
+     origins '*'
 
      resource '*',
        headers: :any,
