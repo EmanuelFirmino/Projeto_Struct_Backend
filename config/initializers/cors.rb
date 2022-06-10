2ad9d254c3bc8f6f9661a94a7f37954b1a1597cb
Victhor-Alexis <victhoralexis123@gmail.com>
2022-06-10 12:52:41 -0300

    Fixing origins cors name

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://padoca.ml'
+     origins 'https://oyster-app-waicl.ondigitalocean.app/'
 
      resource '*',
        headers: :any,
